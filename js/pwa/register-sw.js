229b8867136d9e99dff676e23cf8367d2d23ed67
Arun Ganesh <arun.planemad@gmail.com>
2025-05-16 16:03:34 +0530

    Fix tailwind path

--- js/pwa/register-sw.js
+++ js/pwa/register-sw.js
@@ -3,7 +3,7 @@ if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('/js/pwa/service-worker.js')
       .then((registration) => {
-        console.log('Service Worker registered with scope:', registration.scope);
+        console.debug('Service Worker registered with scope:', registration.scope);
       })
       .catch((error) => {
         console.error('Service Worker registration failed:', error);
